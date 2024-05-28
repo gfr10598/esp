0f7c0d5a0ccd0b8a18f287edacb085c7b3c7330d
Greg Russell <bellwhisperer@gmail.com>
2024-05-28 10:48:42 -0400

    reuse socket - 100 x 2kB packets/sec

--- main/main.cpp
+++ main/main.cpp
@@ -34,15 +34,11 @@ Are UDP packets delivered over WiFi in order?
 #include <lwip/sockets.h>
 #include <esp32-hal.h>
 
-// The sensor produces data at 1920*7*2*4/3 + overhead ~= 36kB/sec.
-// Set up to buffer a comfortable 1.5 seconds of data.
-QueueHandle_t data_queue = xQueueCreate(128, 512);
-
 // Set these to your desired credentials.
 const uint8_t ssid[] = "TheBelfry";
 const uint8_t password[] = "GrandsireCaters";
 
-static const char *TAG = "wifi softAP";
+static const char *SOFT_AP_TAG = "wifi softAP";
 
 static void wifi_event_handler(void *arg, esp_event_base_t event_base,
                                int32_t event_id, void *event_data)
@@ -50,13 +46,13 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
     if (event_id == WIFI_EVENT_AP_STACONNECTED)
     {
         wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
-        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d",
+        ESP_LOGI(SOFT_AP_TAG, "station " MACSTR " join, AID=%d",
                  MAC2STR(event->mac), event->aid);
     }
     else if (event_id == WIFI_EVENT_AP_STADISCONNECTED)
     {
         wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data;
-        ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d, reason=%d",
+        ESP_LOGI(SOFT_AP_TAG, "station " MACSTR " leave, AID=%d, reason=%d",
                  MAC2STR(event->mac), event->aid, event->reason);
     }
 }
@@ -102,8 +98,47 @@ void init_wifi()
     ESP_ERROR_CHECK(esp_wifi_start());
     printf("WiFi started\n");
 }
-void loop()
+
+int socket()
 {
+    int addr_family = AF_INET;
+    int ip_protocol = IPPROTO_IP;
+
+    int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+    if (sock < 0)
+    {
+        return sock;
+    }
+
+    // Set timeout
+    struct timeval timeout;
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
+    return sock;
+}
+
+/**
+ * Sends a message via UDP
+ * @param id - target IP address. Could be a 1-1 (192.168.1.2), could be a broadcast 192.168.1.255
+ * @param port - target port. 0-65000
+ * @param message - message to be sent. Any format. Could be plain text, could be JSON string "{\"text\":\"awesome message\"}"
+ */
+ssize_t udp_client_send_message(int sock, char *ip, int port, char *message)
+{
+    // Set up parameters
+    char rx_buffer[128];
+
+    char host_ip[strlen(ip) + 1];
+    strcpy(host_ip, ip);
+
+    sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr(ip);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(port);
+
+    // Send message
+    return sendto(sock, message, strlen(message), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
 }
 
 const char *udpAddress = "192.168.4.255";
@@ -128,72 +163,19 @@ void set_led(int on)
 {
     gpio_set_level(GPIO_NUM_13, on);
 }
-
-/**
- * Sends a message via UDP
- * @param id - target IP address. Could be a 1-1 (192.168.1.2), could be a broadcast 192.168.1.255
- * @param port - target port. 0-65000
- * @param message - message to be sent. Any format. Could be plain text, could be JSON string "{\"text\":\"awesome message\"}"
- */
-void udp_client_send_message(char *ip, int port, char *message)
-{
-    // Set up parameters
-    char rx_buffer[128];
-
-    char host_ip[strlen(ip)];
-    strcpy(host_ip, ip);
-
-    int addr_family = 0;
-    int ip_protocol = 0;
-
-    sockaddr_in dest_addr;
-    dest_addr.sin_addr.s_addr = inet_addr(ip);
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(port);
-    addr_family = AF_INET;
-    ip_protocol = IPPROTO_IP;
-
-    int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
-    if (sock < 0)
-    {
-        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-        return;
-    }
-
-    // Set timeout
-    struct timeval timeout;
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
-    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
-
-    ESP_LOGI(TAG, "Socket created, sending to %s:%d", ip, port);
-
-    // Send message
-    int err = sendto(sock, message, strlen(message), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-    if (err < 0)
-    {
-        ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-    }
-    else
-    {
-        ESP_LOGI(TAG, "Message sent: %s", message);
-    }
-
-    // Close socket
-    if (sock != -1)
-    {
-        shutdown(sock, 0);
-        close(sock);
-    }
-}
-
-long unsigned blast(char *msg)
+long unsigned blast(int sock, char *msg)
 {
     unsigned long start = micros();
     // Send a UDP packet to the broadcast address.
-    udp_client_send_message("192.168.4.255", 3333, msg);
+    ssize_t sent = udp_client_send_message(sock, "192.168.4.255", 3333, msg);
+    if (sent < 0)
+    {
+        return sent;
+    }
     return micros() - start;
 }
+
+char msg[2049];
 
 extern "C" void app_main()
 {
@@ -213,32 +195,52 @@ extern "C" void app_main()
 
     esp_log_level_set("*", ESP_LOG_WARN);
 
-    char msg[1025];
     for (int i = 0; i < 1024; i++)
     {
         msg[i] = 'A' + i % 64;
     }
-    msg[1024] = 0;
+    msg[2048] = 0;
+
+    int sock = socket();
+    if (sock < 0)
+    {
+        ESP_LOGE(SOFT_AP_TAG, "Unable to create socket: errno %d", errno);
+        while (1)
+            ;
+    }
 
     unsigned long last = 0;
     for (int cnt = 0; cnt < 100000; cnt++)
     {
-        // With delay of 9, we get occasional dropouts??
-        // This produces almost exactly 200 packets per second.
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-        unsigned long blast_time = blast(msg);
+        // Reusing the socket, and sending 2048 byte messages, we can send 90 messages/sec.
+        // With a higher message rate, we start getting send errors.
+        vTaskDelay(11 / portTICK_PERIOD_MS);
+        ssize_t blast_time = blast(sock, msg);
+        if (blast_time < 0)
+        {
+            if (errno == ENOMEM)
+                ESP_LOGE(SOFT_AP_TAG, "ENOMEM");
+            else
+                ESP_LOGE(SOFT_AP_TAG, "Error occurred during sending: errno %d", errno);
+        }
 
         if (cnt % 100 == 0)
         {
             set_led(cnt % 200);
             unsigned long now = micros();
             unsigned long avg = (now - last) / 100;
-            unsigned long throughput = 1000000 / avg; // kB/sec
+            unsigned long throughput = 2000000 / avg; // kB/sec
             last = now;
             timeval datetime;
             gettimeofday(&datetime, NULL);
-            printf("blast at %lld took %lu usecs, average elapsed: %lu throughput: %lu kB/sec\n", datetime.tv_sec, blast_time, avg, throughput);
+            printf("blast at %lld took %d usecs, average elapsed: %lu throughput: %lu kB/sec\n", datetime.tv_sec, blast_time, avg, throughput);
         }
+    }
+    // Close socket
+    if (sock != -1)
+    {
+        shutdown(sock, 0);
+        close(sock);
     }
     while (1)
         ;
